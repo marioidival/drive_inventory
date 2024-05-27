b0a8acdcc81d8aa56cf08a0bb946635169fcb1ed
Mário Idival <marioidival@gmail.com>
2024-05-26 22:24:11 -0300

    chore: attempt to create webhook entrypoint

--- drive_inventory/bootstrap.py
+++ drive_inventory/bootstrap.py
@@ -1,1 +1,21 @@
+import inspect
+from typing import Callable
+from drive_inventory.adapters.notifications import (
+    AbstractNotifications,
+    EmailNotifications,
+)
+from drive_inventory.adapters.repository import AbstractRepository, MongoRepository
+from drive_inventory.adapters.drive import AbstractDrive, DriveAdapter
+from drive_inventory.config import load_credentials
 
+
+def bootstrap(
+    drive_adapter: AbstractDrive,
+    mail_adapter: AbstractNotifications,
+    repo: AbstractRepository,
+):
+    return {
+        "drive": drive_adapter,
+        "mail": mail_adapter,
+        "repo": repo,
+    }
