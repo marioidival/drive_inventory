f2191aebab579780c2837ae441fe14b826ce6838
Mário Idival <marioidival@gmail.com>
2024-05-26 22:22:57 -0300

    chore: create domain module

--- drive_inventory/domain/model.py
+++ drive_inventory/domain/model.py
@@ -1,1 +1,22 @@
+from dataclasses import asdict, dataclass
+from typing import Optional, List, Set
 
+
+@dataclass
+class File:
+    external_id: str
+    filename: str
+    file_extension: Optional[str]
+    mimetype: str
+    owned_by_me: bool
+    checksum: str
+    visibility: str
+    owner_file: Optional[str]
+    updated_at: str
+
+
+@dataclass
+class Log:
+    file_id: str
+    changes: dict
+    timestamp: str
